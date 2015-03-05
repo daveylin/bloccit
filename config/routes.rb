71389476221a164ed35ce8aeb01ac7296a176260
David <daveylin@gmail.com>
2015-03-05 08:18:10 +0000

    Complete crud assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'advertisements/index'
+
+  get 'advertisements/show'
+
   #get 'posts/index'
   #get 'posts/show'
   #get 'posts/new'
   #get 'posts/edit'
   
   resources :posts
+  
+  resources :advertisements
 
   #get 'welcome/contact'
   #get 'welcome/index'
