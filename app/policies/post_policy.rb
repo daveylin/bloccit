0793994fa3a2b491bf757f179af577edd24749dc
David <daveylin@gmail.com>
2015-03-25 22:50:04 +0000

    Authorization assignment issues

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,20 @@
 class PostPolicy < ApplicationPolicy
+  
   def index?
-  true
+    true
   end
+
+  class Scope < Scope
+    #attr_reader :user, :scope
+
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(:published => true)
+      end
+    end
+    
+  end
+  
 end
