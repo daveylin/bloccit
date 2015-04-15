97f5de3bb356e2e20f9fde085ad4cc8b6e2f8797
David <daveylin@gmail.com>
2015-04-15 04:57:55 +0000

    added ability to add comments - no views yet

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @comment = Comment.new(params.require(:comment).permit(:body))
+    @comment.post = Post.find(params[:post_id])
+    @comment.user = current_user
+    #authorize @comment
+    if @comment.save
+       redirect_to [@topic, @post], notice: "Comment was saved successfully."
+     else
+       flash[:error] = "Error creating comment. Please try again."
+       render :new
+     end
+   end
+end
