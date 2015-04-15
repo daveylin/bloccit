cf084bd3e2446f3bd6ccc28863c18aebd7aa5972
David <daveylin@gmail.com>
2015-04-15 20:32:52 +0000

    Complete intro to rspec assignment

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,8 @@
+describe Vote do
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        expect( @post.up_votes ).to eq(1, -1)
+    end
+  end
+end
