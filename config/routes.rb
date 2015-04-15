3a0d1189116aff0a96fe0510702172e8c245dc50
David <daveylin@gmail.com>
2015-04-15 19:00:30 +0000

    complete destroy-assignment

--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,25 @@ Rails.application.routes.draw do
   #resources :posts
   #resources :topics
   #resources :summary
-  resources :topics do
-    resources :posts, except: [:index] do
-      resources :summaries, except: [:index]
-      resources :comments, only: [:create, :destroy]
-    end
-  end
+  
+  #resources :topics do
+    #resources :posts, except: [:index] do
+      #resources :summaries, except: [:index]
+      #resources :comments, only: [:create, :destroy]
+    #end
+  #end
+  
+   resources :topics do
+     resources :posts, except: [:index]
+   end
+ 
+   resources :posts, only: [] do
+     resources :comments, only: [:create, :destroy]
+   end
+  
+   resources :posts, only: [] do
+     resources :summaries, except: [:index]
+   end
 
   
   #get 'welcome/contact'
