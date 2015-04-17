1d8fbf9ae1d5f94e98f222ea84c86951a2479471
David <daveylin@gmail.com>
2015-04-17 05:38:29 +0000

    paginate assignment complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   
+  self.per_page = 50
+  
   default_scope { order('created_at DESC') }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ require 'faker'
  users = User.all
  
  # Create Topics
- 15.times do
+ 100.times do
    Topic.create!(
      name:         Faker::Lorem.sentence,
      description:  Faker::Lorem.paragraph
@@ -30,7 +30,7 @@ require 'faker'
  # The `save` method then saves this User to the database.
 
  # Create Posts
- 50.times do
+ 20000.times do
    Post.create!(
      user:   users.sample,
      topic:  topics.sample,
@@ -41,7 +41,7 @@ require 'faker'
  posts = Post.all
  
  # Create Comments
- 100.times do
+ 1000.times do
    Comment.create!(
      user: users.sample,
      post: posts.sample,
