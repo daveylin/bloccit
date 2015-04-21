0d00ef6bbd1bb62204c6fc7f5f90aa41af12ec87
David <daveylin@gmail.com>
2015-04-21 08:11:48 +0000

    comments fixed added migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418154824) do
-
-  create_table "advertisements", force: true do |t|
-    t.string   "title"
-    t.text     "copy"
-    t.integer  "price"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20150421080511) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
