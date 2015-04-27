444944c42daee66516365d3bd537aa9174aa46ec
David <daveylin@gmail.com>
2015-04-27 08:50:51 +0000

    favoriting assigment complete

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -34,3 +34,4 @@
        @comment.save
      end
    end
+ end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,28 @@ require 'rails_helper'
  describe User do
  
    include TestFactories
- 
+
+   before do
+       @post = associated_post
+       @user = authenticated_user
+    end
+   
    describe "#favorited(post)" do
-     xit "returns `nil` if the user has not favorited the post" do
+     it "returns `nil` if the user has not favorited the post" do
+       expect ( @user.favorited(@post)) == nil
      end
  
-     xit "returns the appropriate favorite if it exists" do
+     it "returns the appropriate favorite if it exists" do
+      @favorite = @user.favorites.build(post: @post)
+      @favorite.save
+      expect( @user.favorited( @post )) == @favorite
+     end
+     
+     it "returns `nil` if the user has favorited another post" do
+      @post2 = associated_post
+      @favorite = @user.favorites.build(post: @post2)
+      @favorite.save
+      expect( @user.favorited( @post )) == nil
      end
    end
  end
