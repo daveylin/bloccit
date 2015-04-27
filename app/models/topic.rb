9c7a29c682bb0a17a31fbcd17a9c7c19c1bfaf06
David <daveylin@gmail.com>
2015-04-27 23:07:51 +0000

    private-topic-checkpoint complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   
   default_scope { order('created_at DESC') }
+  
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
 end
