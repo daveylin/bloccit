0981a6f7f00ca1366a26d1fbc73f4a4b572478ef
David <daveylin@gmail.com>
2015-04-29 21:32:45 +0000

    testing-users-assignment complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,13 +11,14 @@ require 'rails_helper'
      xit "returns the appropriate favorite if it exists" do
      end
    end
-   
+
    describe ".top_rated" do
  
      before do
-       @user1 = create(:user)
-       post = create(:post, user: @user1)
-       create(:comment, user: @user1, post: post)
+       #@user1 = create(:user)
+       #post = create(:post, user: @user1)
+       #create(:comment, user: @user1, post: post)
+       @user1 = create(:user_with_post_and_comment)
  
        @user2 = create(:user)
        post = create(:post, user: @user2)
